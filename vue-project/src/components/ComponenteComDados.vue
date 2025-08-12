4c37c13e59d279ea506e17029830c55f542521a8
Gabriel Yuri <yuriapolinariopessoal@gmail.com>
2025-08-12 20:18:05 -0300

    Primeiras manipulação de dados

--- vue-project/src/components/ComponenteComDados.vue
+++ vue-project/src/components/ComponenteComDados.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <H1>Componente com dados</H1>
-        <p>Nome: Caneta - #123 </p>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }}</p>
+        <p>Altura: {{ altura }}</p>
     </div>
 </template>
     
@@ -9,13 +11,43 @@
 div{
     text-align: center;
     margin: 30px;
-    border: 2px solid rgb(red, green, blue);
-    background-color: #050d04;
+    border: 2px solid rgb(0, 0, 0);
+    background-color: grey;
     border-radius: 15px;
     padding: 10px;
+    font-family: 'Courier New', Courier, monospace;
 }
+
+p{
+    font-size: 30px;
+    font-family: 'Courier New', Courier, monospace;
+}
+
 </style>
-
-<script>
-
+<script setup>
+const nome = "Pedro";
+const idade = 18;
+const altura = 2.3;
 </script>
+
+<!--
+<script>
+//option API
+//export default{
+//    data: function(){
+//        return{
+//            nomeProduto: "Caderno",
+//            codigoProduto: "#321"
+//        }
+//    }
+//}
+
+// Composition API
+//export default{
+//    setup: function(){
+//        const nomeProduto = "Lapis"
+//        const codigoProduto = "#454"
+//        return {nomeProduto, codigoProduto}
+//    }
+//}
+// </script> -->
